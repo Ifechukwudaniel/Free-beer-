161e5d147bba9f95f63f1a8222b9d1f9583f0c2c
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-05-03 20:12:30 +0100

    removed logo and added gastalt

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-//import "gestalt/dist/css";
+import "gestalt/dist/gestalt.css";
 
 class App extends Component {
   render() {
