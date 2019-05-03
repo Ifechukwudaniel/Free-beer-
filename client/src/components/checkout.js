6068fe7df9566671d5687319f94483abb2c9419a
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-05-03 18:05:34 +0100

    added components signin, signout, checkout component

---
+++ client/src/components/checkout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CheckOut extends Component {
+  render() {
+    return (
+      <div>
+          CheckOut
+      </div>
+    )
+  }
+}
+
+export default CheckOut
