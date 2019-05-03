69239173fa411ec2bb1e3fb38ea4353c0bacb420
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-05-03 18:18:39 +0100

    added routes

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class SignOut extends Component {
+class SignUp extends Component {
   render() {
     return (
       <div>
@@ -10,4 +10,4 @@ class SignOut extends Component {
   }
 }
 
-export default SignOut
+export default SignUp
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import SignUp from './components/signup';
+import SignIn from './components/signin';
+import CheckOut from './components/checkout';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {} from "./components/signin"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+ const Root = () => {
+  return (
+     <Router >
+         <Switch>
+              <Route component ={App} exact path= "/"/>
+              <Route component ={SignIn}  path= "/signin"/>
+              <Route component ={SignUp}  path= "/signup"/>
+              <Route component ={CheckOut}  path= "/checkout"/>
+         </Switch>
+     </Router>
+  )
+}
+
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
