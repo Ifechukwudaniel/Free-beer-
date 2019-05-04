cae3aff4a2002ede5e2f45325d96967a60b2b1e7
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-05-04 23:53:58 +0100

    brew component

---
+++ client/src/components/brew.js
@@ -0,0 +1,118 @@
+import React from "react";
+import Strapi from "strapi-sdk-javascript/build/main";
+import { Box, Heading, Text, Image, Card, Button, Mask } from "gestalt";
+const apiUrl = process.env.API_URL || "http://localhost:1337";
+const strapi = new Strapi(apiUrl);
+
+class Brews extends React.Component {
+  state = {
+    brews: [],
+    brand: "",
+    cartItems
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await strapi.request("POST", "/graphql", {
+        data: {
+          query: `
+          query{
+            brand(id: "${this.props.match.params.brandId}"){
+             _id,
+              Name,
+              brews{
+                _id,
+                Name,
+                Description,
+                Image{
+                  url,
+                }
+              Price,
+              }
+            }
+          }`
+        }
+      });
+      this.setState({
+        brews: response.data.brand.brews,
+        brand: response.data.brand.name
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  render() {
+    const { brand, brews } = this.state;
+
+    return (
+      <Box
+        marginTop={4}
+        display="flex"
+        justifyContent="center"
+        alignItems="start"
+      >
+        {/* Brews Section */}
+        <Box display="flex" direction="column" alignItems="center">
+          {/* Brews Heading */}
+          <Box margin={2}>
+            <Heading color="orchid">{brand}</Heading>
+          </Box>
+          {/* Brews */}
+          <Box
+            dangerouslySetInlineStyle={{
+              __style: {
+                backgroundColor: "#bdcdd9"
+              }
+            }}
+            wrap
+            shape="rounded"
+            display="flex"
+            justifyContent="center"
+            padding={4}
+          >
+            {brews.map(brew => (
+              <Box paddingY={4} margin={2} width={210} key={brew._id}>
+                <Card
+                  image={
+                    <Box height={250} width={200}>
+                      <Image
+                        fit="cover"
+                        alt="Brand"
+                        naturalHeight={1}
+                        naturalWidth={1}
+                        src={`${apiUrl}${brew.Image.url}`}
+                      />
+                    </Box>
+                  }
+                >
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    direction="column"
+                  >
+                    <Box marginBottom={2}>
+                      <Text bold size="xl">
+                        {brew.Name}
+                      </Text>
+                    </Box>
+                    <Text>{brew.Description}</Text>
+                    <Text color="orchid">${brew.Price}</Text>
+                    <Box marginTop={2}>
+                      <Text bold size="xl">
+                        <Button color="blue" text="Add to Cart" />
+                      </Text>
+                    </Box>
+                  </Box>
+                </Card>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      </Box>
+    );
+  }
+}
+
+export default Brews;
